8ab191c6122e54b4116fec7b5dde5555ee3bde58
jonntan <67737040+jonntan@users.noreply.github.com>
2021-08-21 04:06:44 +0800

    Added updatePost controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import Mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res, next) => {
@@ -23,4 +24,20 @@ export const createPost = async (req, res, next) => {
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res, next) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  // Checks if post id is from mongoDB
+  if (Mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).json({ message: "No post with that id" });
+
+  // Set new to true to receive updated version of that post
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+
+  res.json(updatedPost);
 };
