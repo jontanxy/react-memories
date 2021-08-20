9e9b16cbdfaa620274175b20265f5ca2e278fdbd
jonntan <67737040+jonntan@users.noreply.github.com>
2021-08-21 04:10:53 +0800

    Added updatePost data response

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -5,7 +5,7 @@ export const getPosts = () => async (dispatch) => {
     const { data } = await api.fetchPosts();
     dispatch({ type: "FETCH_ALL", payload: data });
   } catch (error) {
-    console.error(error.message );
+    console.error(error.message);
   }
 };
 
@@ -15,5 +15,15 @@ export const createPost = (post) => async (dispatch) => {
     dispatch({ type: "CREATE", payload: data });
   } catch (error) {
     console.error(error.message);
+  }
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
+
+    dispatch({ type: "UPDATE", payload: data });
+  } catch (error) {
+    console.log(error.message);
   }
 };
