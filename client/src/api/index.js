59fd822e8ce445655bc38a641d67a61e5920c91f
jonntan <67737040+jonntan@users.noreply.github.com>
2021-08-22 04:54:34 +0800

    Updated api for heroku api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const url = "http://localhost:8081/posts";
+const url =
+  "https://memories-reactjs.herokuapp.com/posts" ||
+  "http://localhost:8081/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
