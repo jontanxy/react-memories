c3e6ab1f8e6a98af24dce1cd8441a082b5405a93
Jon Tan <ce30116@gmail.com>
2022-04-08 03:35:38 +0800

    Feat: Increased image size limit

--- server/index.js
+++ server/index.js
@@ -30,8 +30,8 @@ mongoose
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(express.json({ limit: "500mb" }));
+app.use(express.urlencoded({ extended: true, limit: "500mb" }));
 
 app.get("/", (req, res, next) => {
   res.status(200).json({
